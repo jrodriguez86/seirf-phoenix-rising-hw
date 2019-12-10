8ea6f1580144bdb9b933784276cfe3fc8a3ecf48
Jose Rodriguez <joserodriguez@Joses-iMac.local>
2019-12-10 17:33:27 -0500

    loops.js

---
+++ loops.js
@@ -0,0 +1,20 @@
+// Create a loop that logs the numbers from 0-99 (ascending)
+for (let i=0; i <= 99; i++) {
+    console.log(i)
+}
+// Create a loop that logs the numbers from 99-0 (descending)
+for (let i=99; i >= 0; i--) {
+    console.log(i)
+}
+// Create a loop that logs the numbers from 49-72 (ascending)
+for (let i=49; i <= 72; i++) {
+    console.log(i)
+}
+// Create a loop that logs the numbers from 81-26 (descending)
+for (let i=81; i >= 26; i--) {
+    console.log(i)
+}
+// Create a loop that logs the numbers from 3-90 that are multiples of 3 (ascending)
+for ( let i=3; i <= 90; i+=3) {
+    console.log(i)
+}
