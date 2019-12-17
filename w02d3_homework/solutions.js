ebd7b4bfd73a6f742a77739e5d3e2d14f72fae19
Jose Rodriguez <joserodriguez@Joses-iMac.local>
2019-12-16 20:48:15 -0500

    Palindrome

---
+++ w02d3_homework/solutions.js
@@ -0,0 +1,26 @@
+///////////
+// Verbal Questions 
+///////////
+
+// 1. Parameter is a variable in the declaration of a function
+// Argument is the value passed to the function
+// 2. Return is used to output a value back when called and 
+// console log is used to inspect the value for debugging
+
+///////////
+// Palindrome
+///////////
+
+// function checkPalindrome(str) {
+//     return true;
+// }
+// Can't get it to come back false... Keep getting True True
+
+// console.log(palindrome("Radar"));
+// console.log(palindrome("Borscht"));
+
+////////////
+// Sum Array
+////////////
+
+
