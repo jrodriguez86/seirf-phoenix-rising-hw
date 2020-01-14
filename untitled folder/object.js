15c7312bf23a699fb67020d395b58d68fa9cd74d
Jose Rodriguez <joserodriguez@Joses-iMac.local>
2020-01-13 21:58:53 -0500

    created Timmys story

--- untitled folder/object.js
+++ untitled folder/object.js
@@ -16,7 +16,7 @@ getPrice() {
 }
 
 class Person {
-    constructor(name) {
+    constructor(name, age, height, weight, mood, hamsters, bankAccount) {
         this.name = name;
         this.age = 0;
         this.height = 0;
@@ -24,6 +24,7 @@ class Person {
         this.mood = 0;
         this.hamsters = [];
         this.bankAccount = 0;
+        let i = 0;
     }
     getName() {
         return this.name
@@ -37,29 +38,36 @@ class Person {
     greet() {
         console.log('My name is ' + this.name);
     }
-    eat() {
-        this.weight = 1;
-        this.mood = 1;
+    eat(i) {
+        this.weight+=i;
+        this.mood+=i;
     }
-    exercise() {
-        this.weight = -1,
+    exercise(i) {
+        this.weight-=i;
     }
-    ageUp() {
-        this.age +1;
-        this.height + 1;
-        this.weight + 1;
-        this.mood - 1;
-        this.bankAccount + 10;
+    
+    ageUp(i) {
+        this.age+=i;
+        this.height+=i;
+        this.weight+=i;
+        this.mood-=i;
+        this.bankAccount+=10;
     }
     buyHamster(hamster) {
         this.hamsters.push(hamster);
-        this.mood + 10;
+        this.mood+=10;
         this.bankAccount - getPrice();
 
     }
 }
 
-const you = new Person();
+const Timmy = new Person('Timmy', 5,);
+Timmy.ageUp(5);
+console.log(Timmy);
 
-you.buyHamster();
+
+
+
+
+
 
