45893274c5a993c05495ebde5e3f53400c8e9e1a
Jose Rodriguez <joserodriguez@Joses-iMac.local>
2020-01-20 22:00:45 -0500

    I added list elements. The code didn't seem very dry. I wonder if there is comething I could do about it Hungry for more section

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -1,29 +1,49 @@
-
+$(() => {
 
 // if (typeof jQuery == 'undefined'){
 //     console.log('oops! I still have to link my jQuery properly!');
 //   } else {console.log('I did it! I linked jQuery and this js file!')};
 
-$(() => {
+
 	// put jQuery in here
 
     const $container = $("#container");
 
 console.log($container);
 
-const $h1 = $("<h1>").text("Hogwarts");
+const $h1 = $("<h1>")
+
+$h1.text("hogwarts");
 
 
 
-const $h2 = $("<h2>").text("Jose");
+const $h2 = $("<h2>");
 
-const $h3 = $("<h3>").text("Hufflepuff");
+$h2.text("Jose");
+
+const $h3 = $("<h3>");
+
+$h3.text("Hufflepuff");
 
 const $h4 = $("<h4>").text("Wade");
 
 $h4.addClass("Owl");
 
+// h4 element with you wand???
 
+$container.append($h1, $h2, $h3, $h4);
+
+const $storage = $('<ul>');
+const $trunk = $("<li>"); 
+
+$storage.append($trunk);
+$trunk.append("butter-beer").addClass("secret");
+$trunk.append("time-turner").addClass("secret")
+$trunk.append("invisibility-cloak").addClass("secret");
+
+$container.append($storage);
+
+// can't figure out how to add the list...
 
 
 
