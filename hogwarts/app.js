743d154dfb625fc7fdd807e9fa4f9993896818e0
Jose Rodriguez <joserodriguez@Joses-iMac.local>
2020-01-22 06:05:54 -0500

    having a lot of trouble creating the ul and li

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -33,19 +33,49 @@ $h4.addClass("Owl");
 
 $container.append($h1, $h2, $h3, $h4);
 
-const $storage = $('<ul>');
-const $trunk = $("<li>"); 
+const $ul = $('<ul>');
+const $li = $("<li>"); 
 
-$storage.append($trunk);
-$trunk.append("butter-beer").addClass("secret");
-$trunk.append("time-turner").addClass("secret")
-$trunk.append("invisibility-cloak").addClass("secret");
+// $("ul").append("<li>Butter  beer</li>");
+// $ul.append($li).text("magic map").addClass("secret");
+// $ul.append($li).text("time turner").addClass("secret");
+// $ul.append($li).text("invisibility cloak").addClass("secret");
+$('ul').append().text("Butter Beer");
+// I'm having trouble creating the UL and LI
 
-$container.append($storage);
+$container.append($ul);
 
-// can't figure out how to add the list...
+const $h5 = $("<h5>");
+  
+  $h5.text("Fall 2017");
+  
+  $("th").text("Day");
+  $("th").text("Classes");
+  
+  const $tr = $("<tr>");
+  const $td = $("<td>");
+  
+  $tr.append($td).text("Monday").addClass("Herbolgy");
+  $tr.append($td).text("Tuesday").addClass("Divination");
+  $tr.append($td).text("Wednesday").addClass("History of Magic");
+  $tr.append($td).text("Thursday").addClass("Charms");
+  $tr.append($td).text("Friday").addClass("Potions");
 
-
+  $h4.removeClass("Master Wand");
+  $trunk[0].remove();
+  $h4.addClass("Master Wand").text("Brand new");
+  $h4[1].css("color", "indigo");
+  $h4[0].detach();
+  $($h4).appendTo('body');
+  
+  const $secret = $(".secret");
+  
+  $secret.hide("slow").delay(2000);
+  $secret.show("slow");
+  
+  // add the class cabbage to your pets leash without replacing your current class on your pets leash???
+  
+  $trunk[0].append("Butter beer");
 
 
 
