3d15c4006877d367947c8bf087c6adeabc60a193
jrodriguez86 <57810392+jrodriguez86@users.noreply.github.com>
2020-05-08 21:07:34 -0400

    Can post new tweets

---
+++ tweetr/tweeter_app_api/app/controllers/tweets_controller.rb
@@ -0,0 +1,22 @@
+class TweetsController < ApplicationController
+    def index
+        tweets = Tweet.all
+        render(json: { tweets }
+    end
+
+    def show
+        tweet = Tweet.find(params[:id])
+        render(json: { tweet: tweet })
+      end
+
+      def create
+        tweet = Tweet.new(tweet_params)
+    
+        if tweet.save
+          render json: { tweet: tweet }
+        else
+          render(status: 422, json: { tweet: tweet, errors: tweet.errors })
+        end
+      end
+end
+
